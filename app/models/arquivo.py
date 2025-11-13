f39ba9ae7aefd9df0ab035081eb2e7441682ce74
Amandasfs <amandsfsantos@gmail.com>
2025-11-13 11:18:50 -0300

    Modelos atualizados e organizados

---
+++ app/models/arquivo.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+class Arquivo:
+    def __init__(self, NB, APS, SeguradoFK, Tipo, Caixa_codigo):
+        self.NB = NB
+        self.APS = APS
+        self.SeguradoFK = SeguradoFK
+        self.Tipo = Tipo
+        self.Caixa_codigo = Caixa_codigo
+        self.data_criacao = datetime.now()
+        self.data_atualizacao = datetime.now()
+    
+    def to_dict(self):
+        return {
+            "NB": self.NB,
+            "APS": self.APS,
+            "SeguradoFK": self.SeguradoFK,
+            "Tipo": self.Tipo,
+            "Caixa_codigo": self.Caixa_codigo,
+            "data_criacao": self.data_criacao.isoformat(),
+            "data_atualizacao": self.data_atualizacao.isoformat()
+        }
+    
+    @staticmethod
+    def from_dict(data):
+        arquivo = Arquivo(
+            data["NB"],
+            data["APS"],
+            data["SeguradoFK"],
+            data["Tipo"],
+            data["Caixa_codigo"]
+        )
+        if "data_criacao" in data:
+            arquivo.data_criacao = datetime.fromisoformat(data["data_criacao"])
+        if "data_atualizacao" in data:
+            arquivo.data_atualizacao = datetime.fromisoformat(data["data_atualizacao"])
+        return arquivo
---
+++ app/models/caixa.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+class Caixa:
+    def __init__(self, codigo, NBI, NBF, prateleira, bloco, andar, corredor):
+        self.codigo = codigo
+        self.NBI = NBI
+        self.NBF = NBF
+        self.prateleira = prateleira
+        self.bloco = bloco
+        self.andar = andar
+        self.corredor = corredor
+        self.Arquivos = []  # lista de arquivos associada à caixa
+        self.data_criacao = datetime.now()
+        self.data_atualizacao = datetime.now()
+    
+    def to_dict(self):
+        return {
+            "codigo": self.codigo,
+            "NBI": self.NBI,
+            "NBF": self.NBF,
+            "prateleira": self.prateleira,
+            "bloco": self.bloco,
+            "andar": self.andar,
+            "corredor": self.corredor,
+            "Arquivos": self.Arquivos,
+            "data_criacao": self.data_criacao.isoformat(),
+            "data_atualizacao": self.data_atualizacao.isoformat()
+        }
+    
+    @staticmethod
+    def from_dict(data):
+        caixa = Caixa(
+            data["codigo"],
+            data["NBI"],
+            data["NBF"],
+            data["prateleira"],
+            data["bloco"],
+            data["andar"],
+            data["corredor"]
+        )
+        if "Arquivos" in data:
+            caixa.Arquivos = data["Arquivos"]  # recupera a lista de arquivos
+        if "data_criacao" in data:
+            caixa.data_criacao = datetime.fromisoformat(data["data_criacao"])
+        if "data_atualizacao" in data:
+            caixa.data_atualizacao = datetime.fromisoformat(data["data_atualizacao"])
+        return caixa
